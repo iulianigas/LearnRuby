3a6d73b9341206b3b681484ef7564e97dc7a8204
Iulian Igas <41596757+iulianigas@users.noreply.github.com>
2022-10-26 23:49:14 +0300

    added solved exercises for Numbers and Booleans section

---
+++ 2_numbers_and_booleans/float_methods.rb
@@ -0,0 +1,13 @@
+p 10.5.floor 
+p 10.5.ceil 
+
+puts
+
+p 3.14159.round(2)
+p 3.14159.round(3)
+p 3.14159.round(4)
+
+puts 
+
+p 35.67.abs
+p -35.67.abs
---
+++ 2_numbers_and_booleans/inequality_operator.rb
@@ -0,0 +1,12 @@
+p 10 != 5
+p 10 != 10
+
+puts "--------------"
+
+p "Hello" != "Goodbye"
+p "Hello" != "Hello"
+p "Hello" != "hEllo"
+
+puts "--------------"
+
+p "123" != 123
